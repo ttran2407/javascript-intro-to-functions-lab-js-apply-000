df42c66e8246b74b561e1b7a3743a2095317e9d7
tientran2407.vn@gmail.com <tientran2407.vn@gmail.com>
2018-09-10 15:51:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,18 +23,21 @@ function logWhisper (string) {
 function sayHiToGrandma (string) {
   //creat newString is equal string
   var newString = string;
-  newString = newString.toLowerCase();
-  //convert string to array to compare each word in the string
-  var arr1 = string.split("");
-  var arr2 = newString.split("");
-  var lowerCase = 1;
-  //loop through array to find out if string is lowercase
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      break;
-    }
-    lowerCase = 0;
+  //creat lowerCaseString and UpperCaseString
+  var lowerCase = newString.toLowerCase();
+  var upperCase = newString.toUpperCase();
+  // compare the lowerCase and UpperCase with string, respectively
+  if (lowerCase === string) {
+    return "I can't hear you"
   }
-  // if lowerCase is 0, string contains at least one upperCase word
-  if (lowerCase = 1) {return "I can't hear you!"}
+  if (upperCase === string) {
+
+    return "YES INDEED!"
+  }
+
+  if (string === "I love you, Granda.") {
+
+    return "I love you, too."
+  }
+
 }
